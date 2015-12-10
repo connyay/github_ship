df6cbbab989bfe2581e712c362b62613bc61e4ad
Connor Hindley <connor.hindley@tanium.com>
2015-12-10 12:25:41 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+
+(function() {
+    'use strict';
+
+    var boatRegex = /\/\*!?(?:\r\n|\n)([\s\S]*?)(?:\r\n|\n)[ \t]*\*\//;
+
+    var boats = [
+function() {/*
+             |    |    |
+             )_)  )_)  )_)
+            )___))___))___)\
+           )____)____)_____)\\
+         _____|____|____|____\\\__
+---------\                   /---------
+  ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+    ^^^^      ^^^^     ^^^    ^^
+         ^^^^      ^^^
+*/},
+function() {/*
+     _
+    /|\
+   /_|_\
+ ____|____
+ \_o_o_o_/
+~~ |     ~~~~~
+___t_________ 
+*/},
+function() {/*
+     __/\__
+  ~~~\____/~~~~~~
+    ~  ~~~   ~. 
+*/}
+];
+
+    var shipImg = buildShipImage();
+
+    var shipBtn = document.createElement('button');
+    shipBtn.className = 'btn';
+    shipBtn.style.float = 'left';
+    shipBtn.appendChild(shipImg);
+
+    shipBtn.onclick = function(ev) {
+        ev.preventDefault();
+        var commentField = document.getElementById('new_comment_field');
+        // Get a random boat
+        var boat = boats[Math.floor(Math.random()*boats.length)];
+        // Jam it in
+        var asciiBoat = boatRegex.exec(boat.toString())[1];
+        var val = commentField.value;
+        if (val) {
+            val += '\n';
+        }
+        commentField.value = val + ('```\n' + asciiBoat + '\n```');
+    };
+
+    var commentBtns = document.getElementById('partial-new-comment-form-actions');
+    commentBtns.appendChild(shipBtn);
+
+    function buildShipImage() {
+        var img = document.createElement('img');
+        img.className = 'emoji';
+        img.title= ':ship:';
+        img.alt= ':ship:';
+        img.src = 'https://assets-cdn.github.com/images/icons/emoji/unicode/1f6a2.png';
+        img.height = '20';
+        img.width = '20';
+        img.align = 'absmiddle';
+        return img;
+    }
+}());
